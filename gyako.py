0bc343d87d1a8cb14658f1abe5635c6bd06f9db6
Netti0123 <anettdanko9852@gmail.com>
2022-11-07 17:14:24 +0100

    gkjmn

--- gyako.py
+++ gyako.py
@@ -1,6 +1,10 @@
-a =int(input("ird be a szamot:"))
+import random
+
+a =random.randint(1,100)
+print("A szam",a)
 os = 0
-for x in range(1,a+1):
+while(os!=2):
+    for x in range(1,a+1):
     if a%x==0:
         os+=1
 if os==2:
