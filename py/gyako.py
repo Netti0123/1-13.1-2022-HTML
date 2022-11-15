31b297f20dc6dee782924c5edd36e25aece42123
Netti0123 <anettdanko9852@gmail.com>
2022-11-15 08:20:16 +0100

    py feladat

--- py/gyako.py
+++ py/gyako.py
@@ -1,10 +1,10 @@
 import random
-
-a =random.randint(1,100)
-print("A szam",a)
-os = 0
 while(os!=2):
-    for x in range(1,a+1):
+    a =random.randint(1,100)
+    print("A szam",a)
+    os = 0
+
+for x in range(1,a+1):
     if a%x==0:
         os+=1
 if os==2:
